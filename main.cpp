02d0ec4207710cbce20b5982f1a9ca77ce12688b
orangepi <andrejordan81@yahoo.com>
2025-03-09 19:01:58 +0000

    a

--- main.cpp
+++ main.cpp
@@ -5,25 +5,95 @@
 #include <GL/freeglut.h>
 #include <SOIL/SOIL.h>
 
-GLuint texture = 0;
+GLuint player_texture, background_texture = 0;
+float player_posX , player_posY = 0.0f;
+const int numPontos = 5;
+float posicoes[numPontos][2] = {
+    {-0.6f, 0.6f},
+    {-0.3f, -0.4f},
+    {0.5, 0.5},
+    {0.8, -0.6},
+    {0.0, 0.0}
+};
+
+void load_texture(GLuint* texture,const char* image_location){
+
+    //glEnable(GL_TEXTURE_2D);
+    if(!*texture){
+
+        *texture = SOIL_load_OGL_texture(image_location, SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+        if(!*texture){
+            printf("Erro: '%s'", SOIL_last_result());
+        }
+    }
+    //glDisable(GL_TEXTURE_2D);
+
+    glBindTexture(GL_TEXTURE_2D, *texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void city(){
+
+    glPointSize(10);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    
+    glBegin(GL_POINTS);
+    for (int i = 0; i < numPontos; i++){
+        glVertex2f(posicoes[i][0], posicoes[i][1]);
+    }
+    glEnd();
+}
+
+void background(){
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, background_texture);
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0); glVertex2f(-1.5, -1.5);
+    glTexCoord2f(1.0, 0.0); glVertex2f(1.5, -1.5);
+    glTexCoord2f(1.0, 1.0); glVertex2f(1.5, 1.5);
+    glTexCoord2f(0.0, 1.0); glVertex2f(-1.5, 1.5);
+    glEnd();
+
+    glDisable(GL_TEXTURE_2D);
+
+}
+
+void player(){
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, player_texture);
+    
+    glPushMatrix();
+    glLoadIdentity();
+
+    glTranslatef(player_posX, player_posY, 0.0f);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f); glVertex2f(0.0f, 0.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex2f(0.5f, 0.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex2f(0.5f, 0.5f);
+    glTexCoord2f(0.0f, 1.0f); glVertex2f(0.0f, 0.5f);
+    glEnd();
+
+    glPopMatrix();
+
+    glDisable(GL_TEXTURE_2D);
+
+}
 
 void display(void){
 
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0, 0.0); glVertex2f(0.0f, 0.0f);
-    glTexCoord2f(1.0, 0.0); glVertex2f(0.5f, 0.0f);
-    glTexCoord2f(1.0, 1.0); glVertex2f(0.5f, 0.5f);
-    glTexCoord2f(0.0, 1.0); glVertex2f(0.0f, 0.5f);
-    glEnd();
-
-    glDisable(GL_TEXTURE_2D);
+    background();
+    city();
+    player();
     
     glutSwapBuffers();
 
@@ -38,6 +108,29 @@ void reshape(int width, int height){
     glMatrixMode(GL_MODELVIEW);
 }
 
+void keyboard(unsigned char key, int x, int y){
+
+    switch(key){
+        
+        case 27:
+            exit(0);
+            break;
+
+        case 'w':
+            player_posY += 0.05f; break;
+            
+        case 's':
+            player_posY -= 0.05f; break;
+
+        case 'a':
+            player_posX -= 0.05f; break;
+
+        case 'd':
+            player_posX += 0.05f; break;
+    }
+    glutPostRedisplay();
+}
+
 int main(int argc, char** argv){
 
     glutInit(&argc, argv);
@@ -46,19 +139,14 @@ int main(int argc, char** argv){
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Jogo");
 
-    glEnable(GL_TEXTURE_2D);
-    if(!texture){
-
-        texture = SOIL_load_OGL_texture("char.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-        if(!texture){
-            printf("Erro: '%s'", SOIL_last_result());
-        }
-    }
-    glDisable(GL_TEXTURE_2D);
+    load_texture(&background_texture, "brasil.png");
+    load_texture(&player_texture, "char.png");
 
     glutDisplayFunc(display);
     glutIdleFunc(display);
     glutReshapeFunc(reshape);
+
+    glutKeyboardFunc(keyboard);
 
     glutMainLoop();
 
